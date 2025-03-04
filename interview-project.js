180dbea390c50f32b327ab0fa9538f1299175a14
Marc Joseph <imtweetix@gmail.com>
2025-03-04 15:09:27 -0500

    Fetch the content map from the static URL

--- interview-project.js
+++ interview-project.js
@@ -1,1 +1,9 @@
-// Activity Connection Interview Project
+/* Activity Connection Interview Project */
+
+(async () => {
+  // Fetch the content map from the static URL
+  const url = 'https://interview.actcon.info/static/content_map.json';
+  const contentMapResponse = await fetch(url);
+  // Parse the JSON response
+  const contentMap = await contentMapResponse.json();
+})();
